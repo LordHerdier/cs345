5c3206008d30f8563949f7b05fe948b57087a4c4
Charlie Whittleman <brennwh@siue.edu>
2025-06-04 18:38:17 -0500

    Added main menu :)

--- example.py
+++ example.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
-print("Goodbye world 😈")
+#print("Goodbye world 😈")
 # TODO: Update the message
 
-print("*Explosion Sounds*")
+#print("*Explosion Sounds*")
+
+
+print("Okay just pretend that there's a main menu here")
